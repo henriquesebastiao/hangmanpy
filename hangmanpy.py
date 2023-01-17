0ab1814e43475d71c563a16fc4b4e7fa042429d0
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-01-16 20:18:30 -0400

    *

--- hangmanpy.py
+++ hangmanpy.py
@@ -1,7 +1,33 @@
 from random import random
 import modules.random as random
 
+tentaivas = 3
 palavra = random.sorteiaPalavra()
+palavraOculta = '_' * len(palavra)
+print(palavraOculta)
 
-print('_' * len(palavra))
+while True:
+    while True:
+        letra = input('Informe uma letra: ')
+        if len(letra) > 1:
+            print('Ah espertinho. Voce não pode inserir mais de uma letra por vez.')
+            print('Tente novamente.')
+            print()
+        else:
+            break
+    
+    letra = letra.upper
+    contador = 0
+    for l in palavra:
+        if l == letra:
+            palavraOculta.replace(palavraOculta[contador], l)
+        contador += 1
+    
+    print(palavraOculta)
+    print()
+    
+    if '_' not in palavraOculta:
+        break
 
+print('Você conseguiu desvendar a palavra!')
+print(palavraOculta)
